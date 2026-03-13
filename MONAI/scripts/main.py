b042f917e2852a8d8067b82e80b9452c146d7429
ClairCrest <topwarapol8850@gmail.com>
2026-03-13 16:09:34 +0700

    Plug in Config.py to main.py and change CLI input method

--- MONAI/scripts/main.py
+++ MONAI/scripts/main.py
@@ -1,5 +1,7 @@
 import argparse
+import tempfile
 from src.training.train import train
+from src.utils.configs import build_effective_config, save_config
 
 def main():
     parser = argparse.ArgumentParser(description = "Autosegmentation and reconstruction")
@@ -12,9 +14,36 @@ def main():
                         type = str, 
                         default = "unet",
                         help = "Model name")
+
+    parser.add_argument("--meta_path",
+                        type = str,
+                        default = None,
+                        help = "Override data.meta_path from config")
+
+    parser.add_argument("--images_dir",
+                        type = str,
+                        default = None,
+                        help = "Override data.images_dir from config")
+
+    parser.add_argument("--masks_dir",
+                        type = str,
+                        default = None,
+                        help = "Override data.masks_dir from config")
     
     args = parser.parse_args()
-    train(args.config, args.model)
+
+    effective_config = build_effective_config(
+        config_path = args.config,
+        meta_path = args.meta_path,
+        images_dir = args.images_dir,
+        masks_dir = args.masks_dir,
+    )
+
+    with tempfile.NamedTemporaryFile(mode = "w", suffix = ".yaml", delete = False) as temp_config:
+        save_config(effective_config, temp_config.name)
+        effective_config_path = temp_config.name
+
+    train(effective_config_path, args.model)
 
 if __name__ == "__main__":
     main()
--- MONAI/src/utils/configs.py
+++ MONAI/src/utils/configs.py
@@ -1,8 +1,64 @@
 import yaml
 from monai.utils import set_determinism
 
-with open("configs/configs.yaml", "r") as f:
-    config = yaml.safe_load(f)
+DEFAULT_CONFIG_PATH = "configs/configs.yaml"
+
+
+def load_config(config_path = DEFAULT_CONFIG_PATH):
+    with open(config_path, "r") as f:
+        return yaml.safe_load(f)
+
+
+config = load_config()
+
+
+def apply_data_path_overrides(config_data, meta_path = None, images_dir = None, masks_dir = None):
+    data = config_data.setdefault("data", {})
+
+    if meta_path is not None:
+        data["meta_path"] = meta_path
+    if images_dir is not None:
+        data["images_dir"] = images_dir
+    if masks_dir is not None:
+        data["masks_dir"] = masks_dir
+
+    return config_data
+
+
+def build_effective_config(
+    config_path = DEFAULT_CONFIG_PATH,
+    meta_path = None,
+    images_dir = None,
+    masks_dir = None
+):
+    config_data = load_config(config_path)
+    return apply_data_path_overrides(
+        config_data,
+        meta_path = meta_path,
+        images_dir = images_dir,
+        masks_dir = masks_dir,
+    )
+
+
+def save_config(config_data, output_path):
+    with open(output_path, "w") as f:
+        yaml.safe_dump(config_data, f, sort_keys = False)
+
+
+def set_runtime_config(
+    config_path = DEFAULT_CONFIG_PATH,
+    meta_path = None,
+    images_dir = None,
+    masks_dir = None
+):
+    global config
+    config = build_effective_config(
+        config_path = config_path,
+        meta_path = meta_path,
+        images_dir = images_dir,
+        masks_dir = masks_dir,
+    )
+    return config
 
 
 def get_seed():
