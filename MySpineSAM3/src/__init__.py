73953db002fd9f0886d74d72c8c9fe2faf01b726
Turbulentguy <sawittngam2@gmail.com>
2026-04-30 14:25:15 +0700

    Delete unuse files

--- MySpineSAM3/src/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-MySpineSAM3 Source Package
-==========================
-Modular spine segmentation framework supporting:
-- 3D MONAI models (UNET, UNETR, SwinUNETR)
-- 2D SAM-based models (MobileSAM, SegmentAnyBone)
-"""
-
-__version__ = "1.0.0"
